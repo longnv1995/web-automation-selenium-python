1289c908ffcd32ca668f86758abbec1e2a0c57ef
longnv1995 <longnguyenvan995@gmail.com>
2023-03-17 22:27:06 +0700

    refactor webdriverfactory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
+import os
+
+from libs.webdrivers import WebDriverFactory
+from utils.helpers import load_env_file
+
+
+load_env_file()
+BASE_URL = os.getenv('DEV_BASE_URL')
 
 @pytest.fixture(scope='session')
 def browser(request):
@@ -20,21 +24,12 @@ def headless(request):
 @pytest.fixture(scope='function')
 def driver(browser, url, headless):
     browser = browser.lower()
-    if browser == 'chrome':
-        options = ChromeOptions()
-        if headless:
-            options.add_argument('--headless')
-        options.page_load_strategy = 'normal' # none,eager, default='normal'
-        # service = ChromeService(executable_path='drivers/chromedriver.exe')
-        _driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) # -> take longer to run (need to investigate more)
-        # _driver = webdriver.Chrome(service=service, options=options)
-    else:
-        raise ValueError('Not supported browser')
-    
+    wdf = WebDriverFactory(browser, headless)
+    _driver = wdf._init_driver()
     if url:
         _driver.get(url)
     else:
-        _driver.get('https://demo.nopcommerce.com/')
+        _driver.get(BASE_URL)
 
     _driver.maximize_window()
     yield _driver
